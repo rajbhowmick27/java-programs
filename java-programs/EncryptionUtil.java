95a418b949ff22c52ea23fb7fcf78b5e245cc26f
rajbhowmick27 <52496816+rajbhowmick27@users.noreply.github.com>
2025-07-06 12:03:04 +0530

    Create EncryptionUtil.java

---
+++ java-programs/EncryptionUtil.java
@@ -0,0 +1,33 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class EncryptionUtil {
+
+    private static final String ALGORITHM = "AES";
+    private static final String SECRET_KEY = "MySecretKey12345"; // 16-char key for AES
+
+    public static String encrypt(String data) {
+        try {
+            SecretKeySpec key = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] encrypted = cipher.doFinal(data.getBytes());
+            return Base64.getEncoder().encodeToString(encrypted);
+        } catch (Exception e) {
+            throw new RuntimeException("Error while encrypting", e);
+        }
+    }
+
+    public static String decrypt(String encryptedData) {
+        try {
+            SecretKeySpec key = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] decrypted = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
+            return new String(decrypted);
+        } catch (Exception e) {
+            throw new RuntimeException("Error while decrypting", e);
+        }
+    }
+}
