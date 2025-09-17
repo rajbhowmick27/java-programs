076cf155f1d145431599021565e16a6014605c94
rajbhowmick27 <52496816+rajbhowmick27@users.noreply.github.com>
2025-09-17 09:48:00 +0530

    Update AvroToJson.java

--- java-programs/AvroToJson.java
+++ java-programs/AvroToJson.java
@@ -1,18 +1,20 @@
-import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.Schema;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.Encoder;
 import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.avro.specific.SpecificRecordBase;
 
 import java.io.ByteArrayOutputStream;
-import java.util.Base64;
 
-public class AvroToBase64 {
-    public static String toBase64(SpecificRecordBase record) throws Exception {
+public class AvroToJson {
+    public static String toJson(SpecificRecordBase record) throws Exception {
+        Schema schema = record.getSchema();
+        DatumWriter<SpecificRecordBase> writer = new SpecificDatumWriter<>(schema);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        SpecificDatumWriter<SpecificRecordBase> writer = new SpecificDatumWriter<>(record.getSchema());
-        BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(out, null);
+        Encoder encoder = EncoderFactory.get().jsonEncoder(schema, out);
         writer.write(record, encoder);
         encoder.flush();
-        return Base64.getEncoder().encodeToString(out.toByteArray());
+        return out.toString("UTF-8");
     }
 }
