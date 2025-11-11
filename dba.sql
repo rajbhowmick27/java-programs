4bb5cdcaf50bf7b1c68becdfc1961e6b650d3a7a
rajbhowmick27 <52496816+rajbhowmick27@users.noreply.github.com>
2025-11-11 13:41:20 +0530

    Update dba.sql

--- dba.sql
+++ dba.sql
@@ -6,3 +6,15 @@ SELECT
     ROUND(SUM(bytes_used) * 100 / SUM(bytes_used + bytes_free), 2) AS pct_used
 FROM v$temp_space_header
 GROUP BY tablespace_name;
+
+SELECT tablespace_name, SUM(bytes_used)/1024/1024 used_mb
+FROM v$temp_space_header
+GROUP BY tablespace_name;
+
+
+SELECT name, ROUND(value/1024/1024,2) mb
+FROM v$pgastat
+WHERE name IN ('total PGA allocated','total PGA inuse','maximum PGA allocated');
+
+
+SELECT * FROM v$sysstat WHERE name LIKE 'workarea executions - %';
